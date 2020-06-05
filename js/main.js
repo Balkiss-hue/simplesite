97268cfd443e0e9fc2c8b8df497cc69035ee69f1
Balkiss-hue <58786205+Balkiss-hue@users.noreply.github.com>
2020-06-05 20:06:55 +0100

    The links works

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// let x=5;
+//     y=10;
+//     console.log (x+y);
+
+// let firstName = "Hamid ";
+//     lastName = "Abdul";
+//     console.log(firstName + lastName);
+
+// document.getElementById ("demo").innerHTML = "I\'m getting javascript";
